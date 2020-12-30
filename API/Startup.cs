5279eefe6bcc0e8cc0fa840b0ac3401af5775f1d
Thodoris <thodoris_politidis@yahoo.gr>
2020-12-30 19:59:59 +0200

    Created an Exception handler middleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -32,10 +33,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
