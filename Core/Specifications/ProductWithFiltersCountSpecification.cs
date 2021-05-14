4b5b625494d34e9870414e4834be2d67234a7e41
Thodoris Politidis <thodoris_politidis@yahoo.gr>
2021-05-14 11:38:53 +0300

    Add Search functionality

--- Core/Specifications/ProductWithFiltersCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersCountSpecification.cs
@@ -6,6 +6,7 @@ namespace Core.Specifications
     {
         public ProductWithFiltersCountSpecification(ProductSpecParams productSpecParams)
             : base(x =>
+                (string.IsNullOrEmpty(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search)) &&
                 (!productSpecParams.BrandId.HasValue || x.ProductBrandId == productSpecParams.BrandId) &&
                 (!productSpecParams.TypeId.HasValue || x.ProductTypeId == productSpecParams.TypeId)
             )
